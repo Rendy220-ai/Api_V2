5bbb48019e906a67e182999ea36c818c74b9a25d
Rendy220-ai <rendyindraperatama789@gmail.com>
2025-04-25 22:32:00 +0700

    Update script.js

--- api-page/script.js
+++ api-page/script.js
@@ -285,3 +285,13 @@ window.addEventListener('scroll', () => {
 function closePopup() {
   document.getElementById('popup').style.display = 'none';
 }
+
+const navbar = document.querySelector('.navbar');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 10) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+});
